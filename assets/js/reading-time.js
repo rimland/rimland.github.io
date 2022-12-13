dc486a27322e95e7497a2f6fcb7fee9bc78194e0
rimland <rimland@163.com>
2022-12-13 16:10:04 +0800

    ...

--- assets/js/reading-time.js
+++ assets/js/reading-time.js
@@ -1,21 +1,29 @@
-(()=>{
-const article = document.querySelector("article.h-entry");
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g;
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.ceil(wordCount / 150);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("post-meta");
-  badge.textContent = `⏱️ 大约需要 ${readingTime} 分钟读完`;
+(() => {
+  const article = document.querySelector("article.h-entry");
+  if (article) {
+    const text = article.textContent;
+    // //英文
+    // const wordMatchRegExp = /[^\s]+/g;
+    // const words = text.matchAll(wordMatchRegExp);
+    // // matchAll returns an iterator, convert to array to get word count
+    // const wordCount = [...words].length;
+    // const readingTime = Math.ceil(wordCount / 200);
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
+    // 中文
+    const blankMatchRegExp = /[\s]+/g;
+    const charCount = text.replace(blankMatchRegExp, '').length;
+    const readingTime = Math.floor(charCount / 500);
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}})();
+    const badge = document.createElement("p");
+    // Use the same styling as the publish information in an article's header
+    badge.classList.add("post-meta");
+    badge.textContent = `⏱️ 大约需要 ${readingTime} 分钟读完`;
+
+    // Support for API reference docs
+    const heading = article.querySelector("h1");
+    // Support for article docs with date
+    const date = article.querySelector("time")?.parentNode;
+
+    (date ?? heading).insertAdjacentElement("afterend", badge);
+  }
+})();
